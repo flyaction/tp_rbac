db9aae5da3f71f79036311d9f24bc9b23e1c4e71
xingdong <xingdong1117@126.com>
2017-10-24 16:50:13 +0800

    日志展示增加搜索功能

--- Admin/Home/Common/function.php
+++ Admin/Home/Common/function.php
@@ -22,3 +22,23 @@ function getMenuIcons(){
         'default'=>'fa fa-desktop',
     );
 }
+//格式化页面搜索的时候
+function formatSearchDate($date){
+    $dateArr = explode(' - ', $date);
+    $beginDate = explode('/', $dateArr[0]);
+    $beginDate = $beginDate[2] . "-" . $beginDate[0] . "-" . $beginDate[1];
+    $endDate = explode('/', $dateArr[1]);
+    $endDate = $endDate[2] . "-" . $endDate[0] . "-" . $endDate[1];
+    return array("beginDate" => $beginDate, "endDate" => $endDate);
+}
+//处理页面搜索要展示的时间
+function returnSearchDate($days){
+    $return = [];
+    $return['endTime'] = strtotime(date('Y-m-d'))+3600*24;
+    $return['beginTime'] = $return['endTime']-3600*24*$days;
+    $return['endTime']--;
+    $return['showDate'] = date('m/d/Y',$return['beginTime']).' - '.date('m/d/Y',$return['endTime']);
+    return $return;
+}
+
+
--- Admin/Home/Controller/LogController.class.php
+++ Admin/Home/Controller/LogController.class.php
@@ -4,8 +4,32 @@ use Think\Controller;
 class LogController extends CommonController {
     public function index(){
         $db_prefix = C('DB_PREFIX');
-        $user = M('admin')->alias('user')->field('user.id,user.username,user.realname,user.logintime,user.status,role.name as rolename')->join('left join '.$db_prefix.'role as role on role.id=user.roleid')->select();
-        $this->assign('user',$user);
+        $search = [];
+        if(IS_POST){
+            $title = I('post.title','','trim');
+            $time = I('post.time');
+            $search['time'] = $time;
+
+            $date = formatSearchDate($time);
+            $startDate = $date['beginDate'].' 00:00:00';
+            $endDate = $date['endDate'].' 23:59:59';
+            $where = 'log.addtime>="'.$startDate.'" and log.addtime <="'.$endDate.'"';
+            if($title){
+                $where .= ' and log.title like "%'.$title.'%"';
+                $search['title'] = $title;
+            }
+        }else{
+            $searchTime = returnSearchDate(7);
+            $startDate = date('Y-m-d H:i:s', $searchTime['beginTime']);
+            $endDate = date('Y-m-d H:i:s',$searchTime['endTime']);
+            $search['time'] = $searchTime['showDate'];
+            $where = 'log.addtime>="'.$startDate.'" and log.addtime <="'.$endDate.'"';
+        }
+        $log = M('admin_log')->alias('log')->field('log.*,admin.username,admin.realname')->join('left join '.$db_prefix.'admin as admin on admin.id=log.admin_id')->where($where)->select();
+
+        $this->assign('search',$search);
+        $this->assign('log',$log);
         $this->display();
+
     }
 }
